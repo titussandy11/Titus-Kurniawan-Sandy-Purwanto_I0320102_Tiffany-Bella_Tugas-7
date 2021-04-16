7f8b5041ad6e0564422bba9e6b9b5ecdd86c758b
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 00:07:17 +0700

    Exercise 7.1

--- I0320102_exercise7.1.py
+++ I0320102_exercise7.1.py
@@ -1,1 +1,14 @@
+#Variabel Global
+nama = 'posi'
+tahun ='2018'
 
+def help():
+    #variabel lokal
+    nama ='Lab Posi'
+    tahun = '18'
+    print('nama= %s' %nama)
+    print('tahun= %s' % tahun)
+
+print('nama= %s' %nama)
+print('tahun= %s' %tahun)
+help()
