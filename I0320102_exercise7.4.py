1e5df489dc1f2940431041ad88d05ada4f3701ac
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 00:12:13 +0700

    Exercise 7.4

--- I0320102_exercise7.4.py
+++ I0320102_exercise7.4.py
@@ -1,1 +1,13 @@
+def panggil(func):
+    return func
 
+
+def titus():
+    return 'ISSACIAN'
+
+
+def main():
+    s = panggil(titus())
+    print(s)
+    if __name__ == '__main__':
+        main()
