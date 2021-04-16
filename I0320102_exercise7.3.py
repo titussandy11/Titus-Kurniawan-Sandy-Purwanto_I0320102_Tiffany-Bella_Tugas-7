01025f5370b029f2f78331e394892bdf78a7b852
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 00:10:51 +0700

    Exercise 7.3

--- I0320102_exercise7.3.py
+++ I0320102_exercise7.3.py
@@ -1,1 +1,18 @@
-
+import time
+def countdown(n):
+    li = (n)
+    def next():
+        r = li(0)
+        li[0] -= 1
+        return r
+    return next
+def main():
+    next = countdown(3)
+    while true :
+        val = next()
+        if val == 0:
+            print('Yok bisa yok!')
+            break
+        print(val, end='')
+        time.sleep(1)
+        main()
