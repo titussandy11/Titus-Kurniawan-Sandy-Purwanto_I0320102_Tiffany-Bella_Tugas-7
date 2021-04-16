f50b96e5d8534563499561cb1a7a0e823f3fe2da
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 00:05:10 +0700

    Soal 2

--- I0320102_soal2_tugas7.py
+++ I0320102_soal2_tugas7.py
@@ -1,1 +1,18 @@
+#Fungsi abs
+x = -1.7
+y = (abs(x))
+print('Hasil dari 3.7 - 2 adalah', y)
 
+#Fungsi min dan max
+x = 70
+y = 60
+z = 85
+a = min(x, y, z)
+b = max(x, y, z)
+print('Nilai tertinggi dalam ujian kali ini diraih oleh Zola dengan nilai', b, 'sedangkan nilai terendah adalah', a)
+
+#Fungsi pow
+a = 3
+b = 4
+hasil = pow(b, a)
+print('Hasil dari', b, 'pangkat', a, 'adalah', hasil)
