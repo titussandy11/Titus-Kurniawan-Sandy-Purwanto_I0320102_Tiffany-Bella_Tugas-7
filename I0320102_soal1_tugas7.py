c7ab62f06648b491a7f04a5463d979639f2426b8
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-16 14:16:36 +0700

    Soal 1

--- I0320102_soal1_tugas7.py
+++ I0320102_soal1_tugas7.py
@@ -1,1 +1,25 @@
+#Fungsi capitalize
+teks = 'titus kurniawan'
+ucapan = 'selamat siang'
+flag = teks.capitalize()
+print(ucapan + ''+ flag)
+
+#Fungsi center
+teks = 'Selamat datang di Kota Surakarta'
+center = teks.center(70, '-')
+print(center)
+
+#Fungsi count
+teks = 'Selamat sore dan selamat datang di Indiemart, silahkan buah segarnya, silahkan rotinya'
+count = (teks.count('silahkan'))
+print(count)
+
+#Fungsi replace
+teks = 'Selamat sore dan selamat datang di Indiemart, silahkan buah segarnya, silahkan rotinya'
+replace = teks.replace('sore', 'malam')
+print(replace)
+
+teks = 'Terimakasih'
+penutup = teks.center(70, '=')
+print(penutup)
 
