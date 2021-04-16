164acd58924d9aa51dff720a14c95ee8458dc48d
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 00:14:50 +0700

    Exercise 7.5

--- I0320102_exercise7.5.py
+++ I0320102_exercise7.5.py
@@ -1,1 +1,19 @@
+def panggil(func):
+    return func
+def helloworld():
+    return 'HELLO WORLD'
+def main():
+    daftarnama = ['Putri, Nana, Vina, Dedi']
+    print('Keadaan Awal')
+    print(daftarnama)
 
+    print('\nMenggunakan sorted():')
+    print(sorted(daftarnama))
+
+    daftarnama.sort(key=lambda n: n.lower())
+
+    print('\nKeadaan Akhir: ')
+    print(daftarnama)
+
+if __name__ == '__main__':
+    main()
