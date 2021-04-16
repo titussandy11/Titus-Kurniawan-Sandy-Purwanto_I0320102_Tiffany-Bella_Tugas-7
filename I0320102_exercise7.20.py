134d4e7dbcd1ac68e8a210a2f535aa9ec0cb3d6e
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-17 01:08:27 +0700

    Exercise 7.20

--- I0320102_exercise7.20.py
+++ I0320102_exercise7.20.py
@@ -1,1 +1,10 @@
-
+import math
+a = 81
+b = 25
+c = 9
+print("a= ",a)
+print("b= ",b)
+print("c= ",c)
+print("nilai sqrt a= ", math.sqrt(a))
+print("nilai sqrt a= ", math.sqrt(b))
+print("nilai sqrt a= ", math.sqrt(b))
